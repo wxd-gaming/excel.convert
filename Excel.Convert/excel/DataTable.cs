cd8b973fddbe12fec280c1e0d843a3552b08ca44
troy.chen <492794628@qq.com>
2021-12-15 23:22:51 +0800

    读取excel文件

---
+++ Excel.Convert/excel/DataTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel.Convert.excel
+{
+    public class DataTable
+    {
+        /// <summary>
+        /// 表名
+        /// </summary>
+        public string table;
+        /// <summary>
+        /// 注释
+        /// </summary>
+        public string comment;
+        public string 
+    }
+}
